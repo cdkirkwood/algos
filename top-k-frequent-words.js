4f8cc87574c794526c9a0046d3c9c95ceff6432f
Connor Kirkwood <cdkirkwood@gmail.com>
2018-10-26 22:38:03 -0400

    k frequent words

---
+++ top-k-frequent-words.js
@@ -0,0 +1,27 @@
+var topKFrequent = function(words, k) {
+  const sortedWords = words.sort()
+  const wordCounter = {}
+  for (let i = 0; i < sortedWords.length; i++) {
+    const curWord = sortedWords[i]
+    if (wordCounter[curWord]) wordCounter[curWord]++
+    else wordCounter[curWord] = 1
+  }
+  const keyValues = Object.entries(wordCounter)
+  const descendingOrder = keyValues.sort((a, b) => {
+    if (b[1] - a[1] === 0) {
+      return b[0] < a[0] ? 1 : -1
+    }
+    else {
+      return b[1] - a[1]
+    }
+  })
+  return descendingOrder
+    .slice(0, k)
+    .reduce((returnArr, curArr) => {
+    returnArr.push(curArr[0])
+    return returnArr
+  }, [])
+};
+
+console.log(topKFrequent(["plpaboutit","jnoqzdute","sfvkdqf","mjc","nkpllqzjzp","foqqenbey","ssnanizsav","nkpllqzjzp","sfvkdqf","isnjmy","pnqsz","hhqpvvt","fvvdtpnzx","jkqonvenhx","cyxwlef","hhqpvvt","fvvdtpnzx","plpaboutit","sfvkdqf","mjc","fvvdtpnzx","bwumsj","foqqenbey","isnjmy","nkpllqzjzp","hhqpvvt","foqqenbey","fvvdtpnzx","bwumsj","hhqpvvt","fvvdtpnzx","jkqonvenhx","jnoqzdute","foqqenbey","jnoqzdute","foqqenbey","hhqpvvt","ssnanizsav","mjc","foqqenbey","bwumsj","ssnanizsav","fvvdtpnzx","nkpllqzjzp","jkqonvenhx","hhqpvvt","mjc","isnjmy","bwumsj","pnqsz","hhqpvvt","nkpllqzjzp","jnoqzdute","pnqsz","nkpllqzjzp","jnoqzdute","foqqenbey","nkpllqzjzp","hhqpvvt","fvvdtpnzx","plpaboutit","jnoqzdute","sfvkdqf","fvvdtpnzx","jkqonvenhx","jnoqzdute","nkpllqzjzp","jnoqzdute","fvvdtpnzx","jkqonvenhx","hhqpvvt","isnjmy","jkqonvenhx","ssnanizsav","jnoqzdute","jkqonvenhx","fvvdtpnzx","hhqpvvt","bwumsj","nkpllqzjzp","bwumsj","jkqonvenhx","jnoqzdute","pnqsz","foqqenbey","sfvkdqf","sfvkdqf"],
+1))
